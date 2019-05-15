6b92b73389f7161ab849b598eb13f6dd510530f8
橋本 陽生 <hashimotoharuki@team-lab.com>
2019-05-15 18:12:10 +0900

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
+
+<style>
+  img.head_img {
+    width: 100%;
+  }
+</style>
