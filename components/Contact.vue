2ac023c3d63b286d9e437ebd647e762455933c5c
Haruki-MacBookPro <coral.reef610@gmail.com>
2019-05-16 23:52:01 +0900

    contactをcomponentとして切り離した

---
+++ components/Contact.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-flex xs12>
+    <v-card flat class="contact">
+      <v-card-text class="text-xs-center display-1 font-weight-bold">
+        <div class="contact-text">
+          <br/>
+          「食」を豊かに。「心」を豊かに。
+        </div>
+      </v-card-text>
+      <v-layout justify-center>
+        <v-flex xs10 sm7 md6 lg5 xl4 text-xs-center>
+          <v-text-field
+            label="氏名"
+            solo
+            background-color="#ebebeb"
+          ></v-text-field>
+          <v-text-field
+            label="メールアドレス"
+            solo
+            background-color="#ebebeb"
+          ></v-text-field>
+          <v-text-field
+            label="お問い合わせタイトル"
+            solo
+            background-color="#ebebeb"
+          ></v-text-field>
+          <v-textarea
+            label="お問い合わせ内容"
+            solo
+          ></v-textarea>
+          <v-btn color="teal" class="white--text font-weight-bold" block large>メール作成</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .contact {
+    background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url('~@/assets/contact.jpg');
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    min-height: 300px;
+    height: 600px;
+  }
+  .contact-text {
+    color: #ebebeb;
+    font-weight: bold;
+    font-size: 18px;
+  }
+  /* スマホ横 */
+  @media screen and (min-width: 481px) {
+    .contact-text {
+      font-size: 28px;
+    }
+  }
+  /* タブレット縦以上 */
+  @media screen and (min-width: 769px) {
+    .contact-text {
+      font-size: 30px;
+    }
+  }
+  /* ブラウザ */
+  @media screen and (min-width: 1025px) {
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,98 +1,81 @@
 <template>
-<v-container fluid grid-list-lg>
-  <v-layout row wrap>
-    <v-flex xs12>
-      <v-card flat class="about">
-        <v-card-title primary-title>
-            <div class="display-1 font-weight-bold title-eng">About</div>
-            <div class="descript"><span class="grey--text">「耕らぼ」について</span></div>
-        </v-card-title>
-        <v-card-text class="card-text">
-          「農」と「食」に関わる活動に取組み、<br/>「食」を豊かに、「心」を豊かにします。<br/><br/>
-
-          <ul type="square">
-            <li>安全で美味しい『食』を提供するため、私たちは常に努力・探求し続けます。</li>
-            <li>野菜づくりは土づくり、人づくり。　自然に感謝し、常に人に感謝の心を　持って活動します。</li>
-          </ul>
-        </v-card-text>
-      </v-card>
-    </v-flex>
-    <v-flex xs12>
-      <v-card flat class="service">
-        <v-card-title primary-title>
-            <div class="display-1 font-weight-bold title-eng">Service and Style</div>
-            <div class="descript"><span class="grey--text">事業内容<span class="desc-devide">|</span>活動スタイル</span></div>
-        </v-card-title>
-        <v-layout wrap>
-          <v-flex xs12 sm6>
-            <v-card flat>
-              <v-img :src="require('@/assets/salad.jpg')"></v-img>
-              <v-card-title primary-title>
-                  <div class="headline font-weight-bold">サービス</div>
-              </v-card-title>
-              <v-card-text class="card-text">
-                お客様の顔が見える販売。
-                直営直売を主とし、関連直売所及び、個人・お昼休み訪問販売なども承ります。
-              </v-card-text>
-            </v-card>
-          </v-flex>
-          <v-flex xs12 sm6>
-            <v-card flat>
-              <v-img :src="require('@/assets/saibai-s.jpg')"></v-img>
-              <v-card-title primary-title>
-                  <div class="headline font-weight-bold">栽培</div>
-              </v-card-title>
-              <v-card-text class="card-text">
-                「施設園芸栽培及び、季節露地栽培をしています。
-                減農薬で安全かつ美味しいお野菜を，栄養価が高く安定した品質で提供いたします。
-                食卓を彩る、新鮮で鮮やかな野菜たちです。
-              </v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-card>
-    </v-flex>
-
+  <v-container fluid grid-list-lg>
+    <v-layout row wrap>
       <v-flex xs12>
-        <v-card flat class="contact">
-          <v-card-text class="text-xs-center display-1 font-weight-bold">
-            <div class="contact-text">
-              <br/>
-              「食」を豊かに。「心」を豊かに。
-            </div>
+        <v-card flat class="about">
+          <v-card-title primary-title>
+              <div class="display-1 font-weight-bold title-eng">About</div>
+              <div class="descript"><span class="grey--text">「耕らぼ」について</span></div>
+          </v-card-title>
+          <v-card-text class="card-text">
+            「農」と「食」に関わる活動に取組み、<br/>「食」を豊かに、「心」を豊かにします。<br/><br/>
+
+            <ul type="square">
+              <li>安全で美味しい『食』を提供するため、私たちは常に努力・探求し続けます。</li>
+              <li>野菜づくりは土づくり、人づくり。自然に感謝し、常に人に感謝の心を持って活動します。</li>
+            </ul>
           </v-card-text>
-          <v-layout justify-center>
-            <v-flex xs10 sm7 md6 lg5 xl4 text-xs-center>
-              <v-text-field
-                label="氏名"
-                solo
-                background-color="#ebebeb"
-              ></v-text-field>
-              <v-text-field
-                label="メールアドレス"
-                solo
-                background-color="#ebebeb"
-              ></v-text-field>
-              <v-text-field
-                label="お問い合わせタイトル"
-                solo
-                background-color="#ebebeb"
-              ></v-text-field>
-              <v-textarea
-                label="お問い合わせ内容"
-                solo
-              ></v-textarea>
-              <v-btn color="teal" class="white--text font-weight-bold" block large>メール作成</v-btn>
+        </v-card>
+      </v-flex>
+      <v-flex xs12>
+        <v-card flat class="service">
+          <v-card-title primary-title>
+              <div class="display-1 font-weight-bold title-eng">Service and Style</div>
+              <div class="descript"><span class="grey--text">事業内容<span class="desc-devide">|</span>活動スタイル</span></div>
+          </v-card-title>
+          <v-layout wrap>
+            <v-flex xs12 sm6>
+              <v-card flat>
+                <v-img :src="require('@/assets/salad.jpg')"></v-img>
+                <v-card-title primary-title>
+                    <div class="headline font-weight-bold">サービス</div>
+                </v-card-title>
+                <v-card-text class="card-text">
+                  お客様の顔が見える販売。
+                  直営直売を主とし、関連直売所及び、個人・お昼休み訪問販売なども承ります。
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-card flat>
+                <v-img :src="require('@/assets/saibai-s.jpg')"></v-img>
+                <v-card-title primary-title>
+                    <div class="headline font-weight-bold">栽培</div>
+                </v-card-title>
+                <v-card-text class="card-text">
+                  「施設園芸栽培及び、季節露地栽培をしています。
+                  減農薬で安全かつ美味しいお野菜を，栄養価が高く安定した品質で提供いたします。
+                  食卓を彩る、新鮮で鮮やかな野菜たちです。
+                </v-card-text>
+              </v-card>
             </v-flex>
           </v-layout>
         </v-card>
       </v-flex>
+      <v-flex xs12>
+        <v-card flat class="about">
+          <v-data-table
+            :headers="headers"
+            :items="farms"
+            class="elevation-1"
+          >
 
-
-  </v-layout>
-</v-container>
-</div>
+          </v-data-table>
+        </v-card>
+      </v-flex>
+      <contact></contact>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import contact from '@/components/Contact.vue'
+export default {
+  components:{
+    contact
+  }
+}
+</script>
 
 <style>
   @media screen and (max-width:480px){
@@ -108,34 +91,6 @@
   .desc-devide {
     margin-left: 10px;
     margin-right: 10px;
-  }
-  .contact {
-    background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url('~@/assets/contact.jpg');
-    background-position: center;
-    background-size: cover;
-    position: relative;
-    min-height: 300px;
-    height: 600px;
-  }
-  .contact-text {
-    color: #ebebeb;
-    font-weight: bold;
-    font-size: 18px;
-  }
-  /* スマホ横 */
-  @media screen and (min-width: 481px) {
-    .contact-text {
-      font-size: 28px;
-    }
-  }
-  /* タブレット縦以上 */
-  @media screen and (min-width: 769px) {
-    .contact-text {
-      font-size: 30px;
-    }
-  }
-  /* ブラウザ */
-  @media screen and (min-width: 1025px) {
   }
 
 </style>
