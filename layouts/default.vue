e878ab95fbbfe473a75230ce4c690eba9183b78b
橋本 陽生 <hashimotoharuki@team-lab.com>
2019-05-16 18:24:50 +0900

    service and styleまで追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -58,7 +58,11 @@
       </v-btn>
     </v-toolbar>
     <div id="home">
-      <img class="head_img" src="/header.jpg"/>
+      <div class="box">
+        <div class="head-text">
+          <p>耕して「食」を豊かに<br/><span class="kokoro">「心」を豊かに</span></p>
+        </div>
+      </div>
     </div>
     <v-content>
       <v-container>
@@ -84,7 +88,7 @@
       :fixed="fixed"
       app
     >
-      <span>&copy; 2019</span>
+      <span>&copy;2019 耕らぼ</span>
     </v-footer>
   </v-app>
 </template>
@@ -111,7 +115,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: '耕らぼ'
     }
   }
 }
@@ -119,6 +123,58 @@ export default {
 
 <style>
   #home {
-    width: 100%;
+    background-image: linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .3)), url('~@/assets/header.jpg');
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    min-height: 300px;
+    height: 60vw;
+  }
+  .box {
+   position: absolute;
+   width: 100%;
+   text-align: center;
+   bottom: 0;
+  }
+  .head-text {
+    margin-left: 10%;
+    text-align: left;
+    color: #ebebeb;
+    font-size: 7vw;
+    font-weight: bold;
+    text-shadow: 0 0 5px #383838;
+  }
+
+  span.kokoro {
+    margin-left: calc(3*7vw);
+  }
+
+  /* スマホ横 */
+  @media screen and (min-width: 481px) {
+    .head-text {
+      font-size: 5vw;
+    }
+    span.kokoro {
+      margin-left: calc(3*5vw);
+    }
+  }
+
+  /* タブレット縦以上 */
+  @media screen and (min-width: 769px) {
+    .head-text {
+      font-size: 6vw;
+    }
+    span.kokoro {
+      margin-left: calc(3*6vw);
+    }
+  }
+  /* ブラウザ */
+  @media screen and (min-width: 1025px) {
+    .head-text {
+      font-size: 61.44px;
+    }
+    span.kokoro {
+      margin-left: calc(3*61.44px);
+    }
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,76 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
+<v-container fluid grid-list-lg>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <v-card flat>
+        <v-card-title primary-title>
+            <div class="display-1 font-weight-bold title-eng">About</div>
+            <div class="descript"><span class="grey--text">「耕らぼ」について</span></div>
+        </v-card-title>
+        <v-card-text class="card-text">
+          「農」と「食」に関わる活動に取組み、<br/>「食」を豊かに、「心」を豊かにします。<br/><br/>
+
+          <ul type="square">
+            <li>安全で美味しい『食』を提供するため、私たちは常に努力・探求し続けます。</li>
+            <li>野菜づくりは土づくり、人づくり。　自然に感謝し、常に人に感謝の心を　持って活動します。</li>
+          </ul>
+        </v-card-text>
+      </v-card>
     </v-flex>
+    <v-flex xs12>
+      <v-card flat>
+        <v-card-title primary-title>
+            <div class="display-1 font-weight-bold title-eng">Service and Style</div>
+            <div class="descript"><span class="grey--text">事業内容<span class="desc-devide">|</span>活動スタイル</span></div>
+        </v-card-title>
+        <v-layout wrap>
+          <v-flex xs12 sm6>
+            <v-card flat>
+              <v-img :src="require('@/assets/sarad.jpg')"></v-img>
+              <v-card-title primary-title>
+                  <div class="headline font-weight-bold">サービス</div>
+              </v-card-title>
+              <v-card-text class="card-text">
+                お客様の顔が見える販売。
+                直営直売を主とし、関連直売所及び、個人・お昼休み訪問販売なども承ります。
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-card flat>
+              <v-img :src="require('@/assets/saibai-s.jpg')"></v-img>
+              <v-card-title primary-title>
+                  <div class="headline font-weight-bold">栽培</div>
+              </v-card-title>
+              <v-card-text class="card-text">
+                「施設園芸栽培及び、季節露地栽培をしています。
+                減農薬で安全かつ美味しいお野菜を，栄養価が高く安定した品質で提供いたします。
+                食卓を彩る、新鮮で鮮やかな野菜たちです。
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-flex>
+
   </v-layout>
+</v-container>
+</div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+<style>
+@media screen and (max-width:480px){
+  .container {
+    padding: 0;
   }
 }
-</script>
-
-<style>
-  img.head_img {
-    width: 100%;
-  }
+.title-eng {
+  margin-right: 10px;
+}
+.descript {
+}
+.desc-devide {
+  margin-left: 10px;
+  margin-right: 10px;
+}
 </style>
