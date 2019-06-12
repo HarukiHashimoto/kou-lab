88b7ab305e808d183953289568cd8dd5af176727
橋本 陽生 <hashimotoharuki@team-lab.com>
2019-06-12 12:03:01 +0900

    Products実装，画像差し替えの必要あり

--- components/Product.vue
+++ components/Product.vue
@@ -1,16 +1,70 @@
 <template>
   <v-flex xs12>
     <v-card flat class="product">
-      <div class="vegi">
-      </div>
-      <div class="vegi_text">
-        ここがテキスト
-      </div>
+      <v-card-title primary-title>
+          <div class="display-1 font-weight-bold title-eng">Products</div>
+          <div class="descript"><span class="grey--text">栽培品目</span></div>
+      </v-card-title>
+      <v-img :src="require('@/assets/salad.jpg')"></v-img>
+      <v-layout wrap justify-center>
+        <template v-for="item of items">
+          <v-flex xs12 sm4>
+            <v-card hover height="160px">
+              <v-card-title primary-title>
+                  <span class="font-weight-bold">{{ item.name }}</span>
+              </v-card-title>
+              <v-card-text class="card-text">
+                {{ item.desc }}
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </template>
+      </v-layout>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          name: '葉菜類',
+          desc: 'キャベツ、レタス、ホウレンソウ、コマツナ、ハクサイ、ミズナ、シュンギク、ミニチンゲンサイ、ニラ'
+        },
+        {
+          name: '花菜類',
+          desc: '茎ブロッコリー、カリフラワー'
+        },
+        {
+          name: '根菜類',
+          desc: 'ダイコン、ニンジン、カブ、ショウガ、ゴボウ'
+        },
+        {
+          name: 'マメ類',
+          desc: '枝豆、スナップエンドウ、ソラマメ、インゲン'
+        },
+        {
+          name: 'イモ類',
+          desc: 'サツマイモ、ジャガイモ、サトイモ'
+        },
+        {
+          name: '果菜類',
+          desc: 'トマト、ナス、キュウリ、オクラ、ズッキーニ、ピーマン、パプリカ、トウガラシ、カボチャ'
+        },
+        {
+          name: '茎菜類',
+          desc: 'アスパラガス、タマネギ、ネギ、ニンニク'
+        },
+        {
+          name: '果樹類',
+          desc: 'カキ、クリ、イチジク、イチゴ、ブドウ'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
